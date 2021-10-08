ecfa176d47217a4f768d853bf159af27b81e7a9f
Elena Sitnikova <lnstnkv2002@gmail.com>
2021-10-08 13:55:30 +0700

    add authorization screen

---
+++ app/src/main/java/com/tsu/itindr/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.tsu.itindr.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tsu.itindr.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
--- app/src/main/java/com/tsu/itindr/start/MainActivity.kt
+++ app/src/main/java/com/tsu/itindr/start/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.tsu.itindr.start
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tsu.itindr.R
+import com.tsu.itindr.registration.RegistrationActivity
+import com.tsu.itindr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewbinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewbinding.root)
+        val intent = Intent(this@MainActivity,RegistrationActivity::class.java)
+        viewbinding.buttonRegister.setOnClickListener { startActivity(intent) }
     }
+
 }
